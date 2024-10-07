3f41a3a83a5240b6e45d34c50d3ee67bd7821f47
Ryan Gael <ryang601@gmail.com>
2024-10-07 11:00:06 +0100

    Update project

---
+++ project.py
@@ -0,0 +1,13 @@
+first_num = input("Enter first number: ")
+second_num = input("Enter second number: ")
+
+# First calculations
+
+print(first_num + second_num)
+print(first_num - second_num)
+print(first_num * second_num)
+if second_num == 0: 
+    print("Denominator is zero")
+else:
+    print(first_num/second_num)
+
